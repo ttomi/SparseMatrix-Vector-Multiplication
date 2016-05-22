58e818ac7e04e7c1e914e4378f30dcb72600832d
Tomi <tomitudor@live.com>
2016-05-23 01:52:15 +0200

    Extracting vectors ot of the SparseM and Multiplication is now done in a seperate class

--- SparseMatrix/Program.cs
+++ SparseMatrix/Program.cs
@@ -1,15 +1,116 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace SparseMatrix
 {
     class Program
     {
+        private static readonly int SIZE = 5;
+
         static void Main(string[] args)
         {
+            int[,] matrix = new int[SIZE, SIZE];
+            int[] vector = new int[SIZE];
+            Random rand = new Random();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = rand.Next(0, matrix.GetLength(1) / 2); j < matrix.GetLength(1); j = j + rand.Next(1, matrix.GetLength(1)))
+                {
+                    matrix[i, j] = rand.Next(1, 10);
+                }
+            }
+
+            for (int a = rand.Next(0, matrix.GetLength(1) / 2); a < matrix.GetLength(1); a = a + rand.Next(1, matrix.GetLength(1)))
+            {
+                vector[a] = rand.Next(1, 10);
+            }
+
+            SparseMatrix sparse = new SparseMatrix(matrix);
+
+
+            Console.Write("Vektor: ");
+            for (int a = 0; a < matrix.GetLength(1); a++)
+            {
+                Console.Write(vector[a] + " ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Matrica: ");
+            Console.WriteLine();
+
+            for (int i1 = 0; i1 < matrix.GetLength(0); i1++)
+            {
+                for (int j1 = 0; j1 < matrix.GetLength(1); j1++)
+                {
+                    Console.Write(matrix[i1, j1] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            /*Console.Write("Value niz: ");
+            for (int z = 0; z < value.Count; z++)
+            {
+                Console.Write(value[z] + " ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Column niz: ");
+
+            for (int f = 0; f < column.Count; f++)
+            {
+                Console.Write(column[f] + " ");
+
+            }
+
+            Console.WriteLine();
+            Console.Write("Row_id niz: ");
+
+            for (int g = 0; g < row_id.GetLength(0); g++)
+            {
+                Console.Write(row_id[g] + " ");
+
+            }
+            Console.WriteLine();*/
+            for (int noOfThreads = 1; noOfThreads < 12; noOfThreads *= 2)
+            {
+                Console.WriteLine("Izvedba s " + noOfThreads + " threada:");
+
+                List<ThreadStart> start = new List<ThreadStart>(noOfThreads);
+                List<Thread> thread = new List<Thread>(noOfThreads);
+                for (int threadId = 0; threadId < noOfThreads; threadId++)
+                {
+                    var id = threadId;
+                    var threads = noOfThreads;
+                    start.Add(delegate { sparse.Multiply(id, threads, vector); });
+                    thread.Add(new Thread(start[threadId]));
+                }
+                for (int threadId = 0; threadId < noOfThreads; threadId++)
+                {
+                    thread[threadId].Start();
+                }
+                for (int threadId = 0; threadId < noOfThreads; threadId++)
+                {
+                    thread[threadId].Join();
+                }
+                Console.WriteLine();
+                foreach (int res in sparse.result)
+                {
+                    Console.Write(res);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
         }
+        /*Console.Write("Result niz: ");
+        for (int a = 0; a < result.Count; a++)
+        {
+            Console.Write(result[a] + " ");
+        }*/
     }
 }
+
---
+++ SparseMatrix/SparseMatrix.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SparseMatrix
+{
+    public class SparseMatrix
+    {
+        List<int> column = new List<int>();
+        List<int> value = new List<int>();
+        List<int> row = new List<int>();
+        public List<int> result = new List<int>();
+
+
+        public SparseMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                bool flag = false;
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] != 0)
+                    {
+                        value.Add(matrix[i, j]);
+                        if (flag == false)
+                        {
+                            row.Add(value.Count - 1);
+                            flag = true;
+                        }
+                        column.Add(j);
+                    }
+                }
+            }
+        }
+
+        public void Multiply(int slice, int noOfThreads, int[] vertex)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            
+            for (int i = slice / noOfThreads; i < row.Count / noOfThreads; i++)
+            {
+                result.Add(0);
+                int j = row[i];
+                if (j < row[(row.Count) - 1])
+                    for (j = row[i];
+                         j < row[i + 1]; j++)
+                    {
+                        result[i] += value[j] * vertex[column[j]];
+                    }
+                else
+                    for (j = row[i];
+                             j < value.Count; j++)
+                    {
+                        result[i] += value[j] * vertex[column[j]];
+                    }
+            }
+
+            sw.Stop();
+            Console.WriteLine("Vrijeme izvršavanja " + (slice + 1) + ". threada: " + sw.Elapsed);
+        }
+
+    }
+}
